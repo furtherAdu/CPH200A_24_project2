31935828e81d22a49bfee32fa411392b25d377cf
XinyangHan <hanxinyang@berkeley.edu>
2024-12-01 01:47:33 +0000

    update running command

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ python scripts/main.py \
     --train True \
     --model_name resnet3d \
     --dataset_name nlst \
-    --nlst.batch_size 1024 \
+    --batch_size 6 \
     --num_workers 0 \
     --use_data_augmentation True \
     --pretraining True
