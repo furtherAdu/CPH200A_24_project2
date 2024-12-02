9c97e5e5d07bbb7cb38960b07e0d70bfd084426b
Adú <adu.matory@gmail.com>
2024-12-02 11:58:25 +0000

    remove block comments

--- src/dataset.py
+++ src/dataset.py
@@ -204,23 +204,6 @@ class NLST(pl.LightningDataModule):
         if self.use_data_augmentation:
             augmentation_transforms = [
                 tio.RandomAffine(degrees=20, p=0.5),
-                # tio.RandomFlip(axes=('LR', 'AP', 'SI'), flip_probability=0.5),
-                # tio.RandomAffine(
-                #     scales=(0.9, 1.1),
-                #     degrees=(-10, 10),
-                #     translation=(-5, 5),
-                #     isotropic=False,
-                #     image_interpolation='linear',
-                #     p=0.5
-                # ),
-                # tio.RandomElasticDeformation(
-                #     num_control_points=(7, 7, 7),
-                #     max_displacement=(5.0, 5.0, 5.0),
-                #     locked_borders=2,
-                #     p=0.5
-                # ),
-                # tio.RandomNoise(mean=0.0, std=(0, 0.1), p=0.25),
-                # # tio.RandomBiasField(coefficients=0.5, p=0.3),
             ]
             self.train_transform = tio.Compose(
                 base_transforms + augmentation_transforms
@@ -238,16 +221,6 @@ class NLST(pl.LightningDataModule):
         self.acc2lungrads = pickle.load(open(self.lungrads_path, "rb"))
         self.valid_exams = set(torch.load(self.valid_exam_path, weights_only=True))
         self.train, self.val, self.test = [], [], []
-
-
-        # import random
-        # total_rows = len(self.metadata)
-        # rows_to_load = int(total_rows * 0.005)
-        
-        # # Randomly sample the rows to load
-        # selected_indices = random.sample(range(total_rows), rows_to_load)
-        # self.metadata = [self.metadata[i] for i in sorted(selected_indices)]
-    
 
         for mrn_row in tqdm.tqdm(self.metadata, position=0):
             pid, split, exams, pt_metadata = (
@@ -313,14 +286,6 @@ class NLST(pl.LightningDataModule):
             # calculate class sample count for each split
             if stage == 'fit':
                 self.train_sampler = WeightedRandomSampler(self.get_samples_weight(self.train), num_samples=len(self.train), replacement=True)
-                # self.val_sampler = WeightedRandomSampler(self.get_samples_weight(self.val), num_samples=300, replacement=False)
-            
-            # if stage == 'validate':
-            #     self.val_sampler = WeightedRandomSampler(self.get_samples_weight(self.val), num_samples=len(self.val), replacement=False)
-
-            # if stage in ['test', 'predict']:
-            #     self.test_sampler = WeightedRandomSampler(self.get_samples_weight(self.test), num_samples=len(self.test), replacement=False)
-
         
         if stage == 'fit':
             self.train = NLST_Dataset(self.train, self.train_transform, **NLST_kwargs)
@@ -442,22 +407,7 @@ class NLST_Dataset(torch.utils.data.Dataset):
 
         # Normalize volume
         x = self.normalize(x)  # Custom normalization (mean=0, std=1)
-        # print(f"Original sample['x'] shape: {x.shape}")
-        # Add batch dimension
-        # x = x.unsqueeze(0)  # Shape: [1, C, D, H, W]
-        # mask = mask.unsqueeze(0)
-        # print(f"Shape after adding batch dimension: {x.shape}")
-        # Resize x and mask to fixed size (e.g., D=32, H=224, W=224)
-        # D_size, H_size, W_size = 32, 224, 224
-        # # D_size, H_size, W_size = 16, 112, 112
-        # x = F.interpolate(x, size=(D_size, H_size, W_size), mode='trilinear', align_corners=False)
-        # mask = F.interpolate(mask, size=(D_size, H_size, W_size), mode='nearest')  # Use 'nearest' for masks
 
-        # print(f"Shape after interpolation: {x.shape}")
-        # Remove batch dimension
-        # x = x.squeeze(0)  # Shape: [C, D_size, H_size, W_size]
-        # mask = mask.squeeze(0)
-        # print(f"Shape after squeezing batch dimension: {x.shape}")
         # Expand channels if needed
         if self.num_channels == 3:
             x = x.repeat(3, 1, 1, 1)  # From (1, D, H, W) to (3, D, H, W)
